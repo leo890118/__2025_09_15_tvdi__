c7aad8470cf7d5b484a3a2e660852de45119c281
徐國堂 <roberthsu2003@gmail.com>
2025-10-23 10:06:28 +0800

    modify

--- lesson9/app.py
+++ lesson9/app.py
@@ -4,7 +4,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import numpy as np
-import json
 
 app = Flask(__name__)
 

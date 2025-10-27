79e93a8197af71465480dba52815ed55b62d9b0a
徐國堂 <roberthsu2003@gmail.com>
2025-10-27 09:40:12 +0800

    modify

--- lesson10/static/js/regression.js
+++ lesson10/static/js/regression.js
@@ -1,2 +1,11 @@
-let chart = null //圖表實體
-let modelData = null //儲存模型資料
+let chart = null; //圖表實體
+let modelData = null; //儲存模型資料
+
+// 頁面戴入完成後才執行
+document.addEventListener('DOMContentLoaded', function(){
+    loadRegressionData();
+});
+
+function loadRegressionData(){
+    console.log("loadRegressionData被執行");
+}

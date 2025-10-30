25ee64e72b68f7fbc75c0404910f99731efabaa9
徐國堂 <roberthsu2003@gmail.com>
2025-10-30 15:59:37 +0800

    modify

---
+++ lesson11/static/js/knn.js
@@ -0,0 +1,9 @@
+// 頁面載入完成後執行
+document.addEventListener('DOMContentLoaded',function(){
+    // 固定使用花瓣長度(2)和花瓣寬度(3)
+    loadKnnData()
+})
+
+function loadKnnData(){
+    console.log("loadKnnData")
+}

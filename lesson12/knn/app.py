e7e664b5a3b15aede6670531874287a862e35483
徐國堂 <roberthsu2003@gmail.com>
2025-11-03 10:40:44 +0800

    modify

--- lesson12/knn/app.py
+++ lesson12/knn/app.py
@@ -26,10 +26,23 @@ def knn_data():
     target_names_zh = ["山鳶尾", "變色鳶尾", "維吉尼亞鳶尾"]
 
     # 取得特徵索引（預設使用花瓣長度和花瓣寬度）
-    print(X.shape)
-    print(y.shape)
-    print(feature_names_zh)
-    print(target_names_zh)
+    feature_x = int(request.args.get('feature_x',2))
+    feature_y = int(request.args.get('feature_y', 3))
+    k_neighbors = int(request.args.get('k',5))
+
+
+    #驗證參數
+    if feature_x < 0 or feature_x >= X.shape[1]:
+        feature_x = 2
+    
+    if feature_y < 0 or feature_y >= X.shape[1]:
+        feature_y = 3
+
+    if k_neighbors < 1 or k_neighbors > 20:
+        k_neighbors = 5
+
+    
+    
     
 
     return jsonify(
